9d9f9eb0481b5f940a800be4ca175ba12a108696
vvvvoid-main <vvvvoid-main@users.noreply.github.com>
2021-10-08 05:46:53 +0000

    deploy: 354b1916ef8c7ba39a64a53f498b2a85e772862c

---
+++ _next/static/6rF7Un64ogrFC3XtChLzN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/63876c1dd666ae620ee1.css","static/chunks/pages/index-dfd873cd5a837d7da815.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
