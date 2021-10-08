9253b0b7a0d530fcdb2636bd531f23f47ef4994b
vvvvoid-main <vvvvoid.main@gmail.com>
2021-10-08 12:01:36 +0900

    add deploy flow

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
 module.exports = {
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/nextjs-githubpages-template" : "",
+  trailingSlash: true,
 }
